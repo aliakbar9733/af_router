4d79df29fa1cff3dfd6cc14a002ec69285ace4df
Aliakbar Soleimani <alisoleimani.dev@gmail.com>
2020-05-08 18:49:19 +0430

    init

---
+++ BaseRouter.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace AF;
+
+use Exception;
+
+class BaseRouter
+{
+    public static self $instance;
+    private array $routes = [self::GET => [],
+        self::POST => []];
+    private const AT_SIGN = "@",
+        CONTROLLER = "controller",
+        METHOD = "method",
+        URI = "uri",
+        REQUEST_METHOD = "REQUEST_METHOD";
+
+    private const ROUTE_NOT_EXISTS = "Route not Exists",
+        CONTROLLER_NOT_EXISTS = "Controller not Exists";
+    private const GET = "GET",
+        POST = "POST";
+
+    private array $controllerNamespaces = [];
+
+    private final function __construct()
+    {
+
+    }
+
+    private final function __clone()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function addRoute($method, $uri, $controller)
+    {
+        $this->routes[$method][] = $this->handleController($controller, $uri);
+        return $this;
+    }
+
+    private function handleController($controller, $uri)
+    {
+        $action = explode(self::AT_SIGN, $controller);
+        return
+            [self::URI => $uri,
+                self::CONTROLLER => $action[0],
+                self::METHOD => $action[1]];
+    }
+
+    public function setControllerNamespace($namespace)
+    {
+        $this->controllerNamespaces[] = $namespace;
+    }
+
+    public function get($uri, $controller)
+    {
+        return $this->addRoute(self::GET, $uri, $controller);
+    }
+
+    public function post($uri, $controller)
+    {
+        return $this->addRoute(self::POST, $uri, $controller);
+    }
+
+    private function findRoute($method, $uri)
+    {
+        $methodRoutes = $this->routes[$method];
+        $route = null;
+        foreach ($methodRoutes as $routes) {
+            if ($routes[self::URI] == $uri) {
+                $route = $routes;
+                break;
+            }
+        }
+        if (!$route)
+            throw new  Exception(self::ROUTE_NOT_EXISTS);
+        return $route;
+    }
+
+    private function getRequestMethod()
+    {
+        return $_SERVER[self::REQUEST_METHOD];
+    }
+
+    public function run()
+    {
+        $request = (object)$_REQUEST;
+        $route = $this->findRoute($this->getRequestMethod(), @$request->af_router_action);
+        $controller = $route[self::CONTROLLER];
+        $method = $route[self::METHOD];
+        $class = null;
+        foreach ($this->controllerNamespaces as $namespace) {
+            $class = "$namespace\\$controller";
+            if (class_exists($class)) {
+                if (method_exists(new $class(), $method)) {
+                    break;
+                }
+            }
+        }
+        if ($class) {
+            $class = new $class();
+            echo $class->$method($request);
+            return;
+        }
+        throw new Exception($controller . " " . self::CONTROLLER_NOT_EXISTS);
+    }
+}
---
+++ facade/Router.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Facade;
+
+use AF\BaseRouter;
+use Exception;
+
+/**
+ * @method static get(string $uri, string $controller)
+ * @method static post(string $uri, string $controller)
+ * @method static setControllerNamespace(string $namespace)
+ */
+class Router
+{
+    public static function __callStatic($name, $arguments)
+    {
+        $baseRouter = BaseRouter::getInstance();
+        if (method_exists($baseRouter, $name)) {
+            return $baseRouter->$name(@$arguments[0], @$arguments[1],
+                @$arguments[2],
+                @$arguments[3]);
+        }
+        throw new Exception($name . "Method not Exists");
+    }
+
+    public static function run()
+    {
+        BaseRouter::getInstance()->run();
+    }
+}
